228ab40ab293285016b3ea25251de4f00ec45b8c
Kyle Bautista <[kylealexanderbautista09@gmail.com]>
2025-03-03 15:02:41 -0500

    user can like a photo, use FavIcon for like button and make a new PhotoFavButton

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,11 +1,19 @@
 // import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
+import { useState } from 'react';
+import FavIcon from './FavIcon';
 
 const PhotoFavButton = () => {
+  const [liked, setLiked] = useState(false);
+
+  const handleClick = () => {
+    setLiked(!liked);
+  };
+
   return (
-    <div className="photo-list__fav-icon">
+    <div className="photo-list__fav-icon" onClick={handleClick}>
       <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+        <FavIcon selected={liked}/>
       </div>
     </div>
   );
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,16 +1,18 @@
 import "../styles/PhotoListItem.scss";
+import PhotoFavButton from "./PhotoFavButton";
 
 const PhotoListItem = ({ username, imageSource, id, location, profile }) => {
   return (
-    <div className="photo-list__item">
-      <img className="photo-list__image" src={imageSource} alt={`Photo by ${username}`} />
+    <div className="photo-list__item" key={id}>
+      <div className="photo-list__image-container">
+        <PhotoFavButton />
+        <img className="photo-list__image" src={imageSource} alt={`Photo by ${username}`} />
+      </div>
       <div className="photo-list__user-details">
         <img className="photo-list__user-profile" src={profile} alt={`Profile of ${username}`} />
         <div className="photo-list__user-info">
-          <p>{username}</p>
-          <div className="photo-list__user-location">
-            <p>{location.city}, {location.country}</p>
-          </div>
+          <p className="photo-list__username">{username}</p>
+          <p className="photo-list__user-location">{location.city}, {location.country}</p>
         </div>
       </div>
     </div>
