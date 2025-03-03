1b9f07d09addb8b65e5e5ad8c770d9d1acaa9d92
Kyle Bautista <[kylealexanderbautista09@gmail.com]>
2025-03-03 14:03:54 -0500

    Add styling to PhotoListItem component, apply styking classes to the PhotoListItem

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,15 +2,19 @@ import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = ({ username, imageSource, id, location, profile }) => {
   return (
-    <div className="photo-list-item">
-      <img src={imageSource} alt={`Photo by ${username}`} />
-      <div className="photo-list-item__details">
-        <img src={profile} alt={`Profile of ${username}`} />
-        <p>{username}</p>
-        <p>{location.city}, {location.country}</p>
+    <div className="photo-list__item">
+      <img className="photo-list__image" src={imageSource} alt={`Photo by ${username}`} />
+      <div className="photo-list__user-details">
+        <img className="photo-list__user-profile" src={profile} alt={`Profile of ${username}`} />
+        <div className="photo-list__user-info">
+          <p>{username}</p>
+          <div className="photo-list__user-location">
+            <p>{location.city}, {location.country}</p>
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default PhotoListItem;
